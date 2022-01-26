cf0d7c9b356c970181cfeeed63460ba27baa81c4
Егор Мясников <erastofen@gmail.com>
2022-01-26 15:42:51 +0300

    set session kye

--- app/__init__.py
+++ app/__init__.py
@@ -9,5 +9,7 @@ def create_app(*args, **kwargs):
     app.debug = True
     app.config['UPLOAD_FOLDER'] = config.UPLOAD_FOLDER
     app.config['BASEDIR'] = Path(__file__).parent.parent
+    app.secret_key = 'ppdf'
+    app.config['SESSION_TYPE'] = 'filesystem'
 
     return app
